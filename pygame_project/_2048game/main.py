b32f55cdb04e9d6cbdb7a7eb29b68c3c6951fb9e
lalhrinthang <lalhrinthang@gmail.com>
2024-04-10 13:38:19 +0800

    Add RowGrid

--- pygame_project/_2048game/main.py
+++ pygame_project/_2048game/main.py
@@ -12,6 +12,9 @@ WIDTH, HEIGHT = 800,800
 ROWS = 4
 COLS = 4
 
+RECT_HEIGHT = HEIGHT // ROWS
+RECT_WIDTH = WIDTH // COLS
+
 OUTLINE_COLOR = (187,170,160)
 OUTLINE_THICKNESS = 10
 BACKGROUND_COLOR = (205,192,180)
@@ -21,6 +24,13 @@ WINDOW = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("2048")
 
 def draw_grid(window):
+
+    for row in range(1,ROWS):
+        y = row * RECT_HEIGHT
+        pygame.draw.line(window,OUTLINE_COLOR,(0,y),(WIDTH,y),OUTLINE_THICKNESS)
+
+
+
     pygame.draw.rect(window,OUTLINE_COLOR,(0,0,WIDTH,HEIGHT),OUTLINE_THICKNESS)
 
 
